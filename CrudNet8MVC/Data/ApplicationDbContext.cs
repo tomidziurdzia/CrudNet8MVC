cb9495915a8fa44f5ec3ea2228499012c74dec75
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2024-07-26 10:41:46 +0200

    Model Contact and configuration conexion db

--- CrudNet8MVC/Data/ApplicationDbContext.cs
+++ CrudNet8MVC/Data/ApplicationDbContext.cs
@@ -1,6 +1,11 @@
+using CrudNet8MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CrudNet8MVC.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        public DbSet<Contact> Contact { get; set; }
 }
